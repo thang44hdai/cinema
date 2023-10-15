4b78f2f63ef2cf2615e68e934e68a6be6af367d3
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2023-10-16 00:11:31 +0700

    .

--- lib/models/api.dart
+++ lib/models/api.dart
@@ -1,6 +1,20 @@
-import 'package:cinema/models/constants.dart';
+import 'dart:convert';
 
-class Api{
-  String trendingMovies = "https://api.themoviedb.org/3/trending/movie/day?api_key=${Constaints.apiKey}";
-  List<Movie>
+import 'package:cinema/models/constants.dart';
+import 'movie.dart';
+import 'package:http/http.dart' as http;
+
+class Api {
+  String trendingUrl =
+      "https://api.themoviedb.org/3/trending/movie/day?api_key=${Constaints.apiKey}";
+
+  Future<List<Movie>> getTredingMovies() async {
+    final response = await http.get(Uri.parse(trendingUrl));
+    if (response.statusCode == 200) {
+      final decodeData = json.decode(response.body)['result'] as List;
+      return decodeData.map((e) => Movie.fromJson(e)).toList();
+    } else {
+      throw Exception("Something happend");
+    }
+  }
 }
